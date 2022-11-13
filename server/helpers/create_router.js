0d28c94ae83d0a158877e57a0dc12f8be0e7d25e
Georgia Hogan <georgiahogan@Georgias-MacBook-Pro.local>
2022-11-13 17:22:11 +0000

    added all routes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -32,6 +32,51 @@ const createRouter = function (collection) {
     });
   });
 
+  // CREATE
+  router.post('/', (req, res) => {
+    const newData = req.body
+    collection.insertOne(newData)
+    .then((result) => {
+      res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err});
+    });
+  });
+
+  // DESTROY
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    collection
+    .deleteOne({_id: ObjectID(id) })
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err })
+    });
+  });
+  
+  // UPDATE
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+    .updatedOne(
+      {_id: ObjectID(id)},
+      { $set: updatedData },
+    )
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err});
+    });
+  })
 
   return router;
 
