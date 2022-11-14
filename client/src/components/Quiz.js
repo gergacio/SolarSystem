56bb59274232e9bb008e5ec5c697569c716e888f
esau fong <esaufong@gmail.com>
2022-11-14 23:50:19 +0000

    additional quiz questions

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -30,18 +30,114 @@ function Quiz() {
   //mock data (questions as arra of)
   const questions = [
     {
-      text: "Closest planet to the sun?",
+      text: "What planet is closest to our Sun?",
       options: [
         { id: 0, text: "Mars", isCorrect: false },
-        { id: 1, text: "Mercury", isCorrect: true }
+        { id: 1, text: "Pluto", isCorrect: false },
+        { id: 2, text: "Venus", isCorrect: false },
+        { id: 3, text: "Titan", isCorrect: false },
+        { id: 4, text: "Mercury", isCorrect: true }
     
       ],
     },
     {
-      text: "Failed star?",
+      text: "Which planet is a failed star?",
       options: [
-        { id: 0, text: "jupiter", isCorrect: true },
-        { id: 1, text: "saturn", isCorrect: false },
+		  { id: 0, text: "Neptune", isCorrect: false },
+		  { id: 1, text: "Mercury", isCorrect: false },
+		  { id: 2, text: "Jupiter", isCorrect: true },
+		  { id: 3, text: "Earth", isCorrect: false },
+        { id: 4, text: "Mars", isCorrect: false },
+      ]
+    },
+    {
+      text: "Which planet is often referred to as the Red Planet?",
+      options: [
+		{ id: 0, text: "Uranus", isCorrect: false },
+		{ id: 1, text: "Mars", isCorrect: true },
+        { id: 2, text: "Saturn", isCorrect: false },
+        { id: 3, text: "Neptune", isCorrect: false },
+        { id: 4, text: "Earth", isCorrect: false },
+      ]
+    },
+    {
+      text: "Which is the coldest planet out of the selection below?",
+      options: [
+        { id: 0, text: "Earth", isCorrect: false },
+        { id: 1, text: "Jupiter", isCorrect: false },
+        { id: 2, text: "Mars", isCorrect: false },
+        { id: 3, text: "Mercury", isCorrect: false },
+        { id: 4, text: "Saturn", isCorrect: true },
+      ]
+    },
+    {
+      text: "Which planet has a radius of 25,362km?",
+      options: [
+        { id: 0, text: "Mars", isCorrect: false },
+        { id: 1, text: "Earth", isCorrect: false },
+        { id: 2, text: "Venus", isCorrect: false },
+        { id: 3, text: "Uranus", isCorrect: true },
+        { id: 4, text: "Jupiter", isCorrect: false },
+      ]
+    },
+    {
+      text: "It takes sunlight 43 minutes to reach which planet?",
+      options: [
+        { id: 0, text: "Jupiter", isCorrect: true },
+        { id: 1, text: "Venus", isCorrect: false },
+        { id: 2, text: "Neptune", isCorrect: false },
+        { id: 3, text: "Uranus", isCorrect: false },
+        { id: 4, text: "Saturn", isCorrect: false },
+      ]
+    },
+    {
+      text: "Which planets have a ring system similar to Saturn?",
+      options: [
+        { id: 0, text: "Earth", isCorrect: false },
+        { id: 1, text: "Mercury", isCorrect: false },
+        { id: 2, text: "Neptune", isCorrect: true },
+        { id: 3, text: "Mars", isCorrect: false },
+        { id: 4, text: "Uranus", isCorrect: true },
+      ]
+    },
+    {
+      text: "Which planet has a 16 hour day?",
+      options: [
+        { id: 0, text: "Saturn", isCorrect: false },
+        { id: 1, text: "Neptune", isCorrect: true },
+        { id: 2, text: "Veuns", isCorrect: false },
+        { id: 3, text: "Mars", isCorrect: false },
+        { id: 4, text: "Earth", isCorrect: false },
+      ]
+    },
+    {
+      text: "Jupiter has how many moons?",
+      options: [
+        { id: 0, text: "7", isCorrect: false },
+        { id: 1, text: "111", isCorrect: false },
+        { id: 2, text: "63", isCorrect: false },
+        { id: 3, text: "39", isCorrect: false },
+        { id: 4, text: "67", isCorrect: true },
+      ]
+    },
+    {
+      text: "How many billions of kilometers is Neptune from the Sun?",
+      options: [
+        { id: 0, text: "4.4737", isCorrect: false },
+        { id: 1, text: "4.4738", isCorrect: true },
+        { id: 2, text: "4.4739", isCorrect: false },
+        { id: 3, text: "4.4740", isCorrect: false },
+        { id: 4, text: "A lot", isCorrect: true },
+      ]
+    },
+    {
+      text: "Starbucks has opened 33,833 stores on which planet?",
+      options: [
+        { id: 0, text: "Pluto", isCorrect: false },
+        { id: 1, text: "Earth", isCorrect: true },
+        { id: 2, text: "Saturn", isCorrect: false },
+        { id: 3, text: "Mars", isCorrect: false },
+        { id: 4, text: "Venus", isCorrect: false },
       ]
     }
   ];
@@ -75,12 +171,12 @@ function Quiz() {
       {showResults ? (
        
         <div>
-          <h1>Final Results</h1>
+          <h1>Final Score</h1>
           <h2>
-            {score} out of {questions.length} correct - (
+            {score} out of {questions.length} correct  (
             {(score / questions.length) * 100}%)
           </h2>
-          <button onClick={() => restartQuizz()}>restart quiz</button>
+          <button onClick={() => restartQuizz()}>RETAKE QUIZ</button>
         </div>
       ) : (
        
